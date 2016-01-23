7c7c6e9d4c075822e42a26fb11fb47c2ece18344
Mark Scherer <euromark@web.de>
2016-01-23 15:15:47 +0100

    Add test case

--- tests/TestCase/View/Helper/HighlighterHelperTest.php
+++ tests/TestCase/View/Helper/HighlighterHelperTest.php
@@ -164,4 +164,42 @@ TEXT;
 		$this->assertTextEquals($expected, $result);
 	}
 
+	/**
+	 * @return void
+	 */
+	public function testHighlightDebug() {
+		$text = <<<'TEXT'
+# hello world
+
+you can write text [with links](http://example.com) inline or [link references][1].
+
+* one _thing_ has *em*phasis
+* two __things__ are **bold**
+
+[1]: http://example.com
+
+---
+
+hello world
+===========
+
+<this_is inline="xml"></this_is>
+
+> markdown is so cool
+
+	so are code segments
+
+1. one thing (yeah!)
+2. two thing `i can write code`, and `more` wipee!
+TEXT;
+
+		$this->Highlighter->config('debug', true);
+
+		$result = $this->Highlighter->highlight($text, ['lang' => 'markdown']);
+		$expected = '<!-- ';
+		$this->assertContains($expected, $result);
+		$expected = 'ms -->';
+		$this->assertContains($expected, $result);
+	}
+
 }
